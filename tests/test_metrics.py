3e4434184bec71b8029ebae5aff54e892af5d5ad
bpesquet <bpesquet@gmail.com>
2020-02-12 22:06:21 +0100

    Remove scikit-learn from dependencies

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -7,34 +7,35 @@ Unit tests for metrics
 
 from math import isclose
 import numpy as np
-from sklearn.metrics import mean_squared_error as mse_sk, accuracy_score
 from pyfit.metrics import mean_squared_error, euclidean_distance, accuracy
 
 
 def test_mse() -> None:
-    expected_list = (np.array([3, -0.5, 2, 7]), np.array([[0.5, 1], [-1, 1], [7, -6]]))
-    predicted_list = (np.array([2.5, 0.0, 2, 8]), np.array([[0, 2], [-1, 2], [8, -5]]))
+    y_true = np.array([3, -0.5, 2, 7])
+    y_pred = np.array([2.5, 0.0, 2, 8])
 
-    for expected, predicted in zip(expected_list, predicted_list):
-        error = mean_squared_error(expected, predicted)
-        error_sk = mse_sk(expected, predicted)
-        assert error == error_sk
+    error = mean_squared_error(y_true, y_pred)
+    assert isclose(error, (0.25 + 0.25 + 1) / 4)
+
+    y_true = np.array([[1, 0, 0, 1], [0, 1, 1, 1], [1, 1, 0, 1]])
+    y_pred = np.array([[0, 0, 0, 1], [1, 0, 1, 1], [0, 0, 0, 1]])
+
+    error = mean_squared_error(y_true, y_pred)
+    assert isclose(error, (1.0 / 3 + 2.0 / 3 + 2.0 / 3) / 4)
 
 
 def test_euclidean_distance() -> None:
-    # a = np.array([1, 1.5])
-    # b = np.array([3, 3])
+    a = np.array([1, 1.5])
+    b = np.array([3, 3])
 
-    # dist = euclidean_distance(a, b)
-    # dist_sk = eucl_dist_sk(a, b)
-    # assert dist == dist_sk
+    dist = euclidean_distance(a, b)
+    assert isclose(dist, np.linalg.norm(a - b))
 
     a = np.random.rand(3, 3)
     b = np.random.rand(3, 3)
 
     dist = euclidean_distance(a, b)
-    dist_sk = np.linalg.norm(a - b)
-    assert isclose(dist, dist_sk)
+    assert isclose(dist, np.linalg.norm(a - b))
 
 
 def test_accuracy() -> None:
@@ -42,5 +43,7 @@ def test_accuracy() -> None:
     y_pred = np.array([0, 2, 1, 3])
 
     acc = accuracy(y_true, y_pred)
-    acc_sk = accuracy_score(y_true, y_pred)
-    assert acc == acc_sk
+    assert acc == 0.5
+
+    acc = accuracy(y_true, y_true)
+    assert acc == 1.0
