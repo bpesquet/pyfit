7d7bbc983bc0b53a8e97a4e74b1a3a0b71e2ffdc
bpesquet <bpesquet@gmail.com>
2020-04-15 19:18:15 +0200

    Add MSE loss

---
+++ pyfit/loss.py
@@ -0,0 +1,23 @@
+"""
+Loss functions
+"""
+
+# pylint: disable=too-few-public-methods
+
+from pyfit.engine import Scalar, Vector
+
+
+class MSELoss:
+    """Mean Squared Error loss"""
+
+    def __call__(self, y_true: Vector, y_pred: Vector) -> Scalar:
+        """Compute MSE loss"""
+
+        total_squared_error: Scalar = sum(
+            (
+                (y_true_i - y_pred_i) * (y_true_i - y_pred_i)
+                for (y_true_i, y_pred_i) in zip(y_true, y_pred)
+            ),
+            Scalar(0),
+        )
+        return total_squared_error / max(len(y_true), 1)
---
+++ tests/test_loss.py
@@ -0,0 +1,17 @@
+"""
+Unit test for loss functions
+"""
+
+# pylint: disable=missing-docstring
+
+from math import isclose
+from pyfit.engine import Scalar, Vector
+from pyfit.loss import MSELoss
+
+
+def test_mse() -> None:
+    y_true: Vector = [Scalar(3), Scalar(-0.5), Scalar(2), Scalar(7)]
+    y_pred: Vector = [Scalar(2.5), Scalar(0.0), Scalar(2), Scalar(8)]
+    loss = MSELoss()
+    error: Scalar = loss(y_true, y_pred)
+    assert isclose(error.data, 0.375)
