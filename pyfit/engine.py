84e44d857a661bd5a4678a3174725471ddba5133
bpesquet <bpesquet@gmail.com>
2020-04-15 19:18:43 +0200

    Add support for - and / operators

--- pyfit/engine.py
+++ pyfit/engine.py
@@ -1,5 +1,5 @@
 """
-Minimalist autograd engine that implements reverse-mode autodifferentiation, aka backpropagation.
+Autograd engine implementing reverse-mode autodifferentiation, aka backpropagation.
 
 Heavily inspired by https://github.com/karpathy/micrograd/blob/master/micrograd/engine.py
 """
@@ -8,6 +8,8 @@ Heavily inspired by https://github.com/karpathy/micrograd/blob/master/micrograd/
 # pylint: disable=protected-access
 
 from typing import Union, Tuple, List, Set, Callable
+
+Vector = List["Scalar"]
 
 
 class Scalar:
@@ -31,8 +33,20 @@ class Scalar:
         out: Scalar = Scalar(self.data + _other.data, (self, _other), "+")
 
         def _backward() -> None:
-            self.grad += out.grad
-            _other.grad += out.grad
+            self.grad += out.grad  # d(out)/d(self) = 1
+            _other.grad += out.grad  # d(out)/d(other) = 1
+
+        out._backward = _backward
+
+        return out
+
+    def __sub__(self, other: Union["Scalar", float]) -> "Scalar":
+        _other: Scalar = other if isinstance(other, Scalar) else Scalar(other)
+        out: Scalar = Scalar(self.data - _other.data, (self, _other), "-")
+
+        def _backward() -> None:
+            self.grad += out.grad  # d(out)/d(self) = 1
+            _other.grad -= out.grad  # d(out)/d(other) = -1
 
         out._backward = _backward
 
@@ -43,8 +57,21 @@ class Scalar:
         out = Scalar(self.data * _other.data, (self, _other), "*")
 
         def _backward() -> None:
-            self.grad += _other.data * out.grad
-            _other.grad += self.data * out.grad
+            self.grad += out.grad * _other.data  # d(out)/d(self) = other
+            _other.grad += out.grad * self.data  # d(out)/d(other) = self
+
+        out._backward = _backward
+
+        return out
+
+    def __truediv__(self, other: Union["Scalar", float]) -> "Scalar":
+        _other = other if isinstance(other, Scalar) else Scalar(other)
+        out = Scalar(self.data / _other.data, (self, _other), "/")
+
+        def _backward() -> None:
+            self.grad += out.grad / _other.data  # d(out)/d(self) = 1/other
+            # d(out)/d(other) = -self/(other*other)
+            _other.grad += out.grad * (-self.data / (_other.data * _other.data))
 
         out._backward = _backward
 
@@ -86,8 +113,16 @@ class Scalar:
     def __radd__(self, other: Union["Scalar", float]) -> "Scalar":
         return self.__add__(other)
 
+    def __rsub__(self, other: Union["Scalar", float]) -> "Scalar":
+        _other = other if isinstance(other, Scalar) else Scalar(other)
+        return _other.__sub__(self)
+
     def __rmul__(self, other: Union["Scalar", float]) -> "Scalar":
         return self.__mul__(other)
+
+    def __rtruediv__(self, other: Union["Scalar", float]) -> "Scalar":
+        _other = other if isinstance(other, Scalar) else Scalar(other)
+        return _other.__truediv__(self)
 
     def __repr__(self) -> str:
         return f"Scalar(data={self.data}, grad={self.grad})"
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -3,17 +3,33 @@ Unit tests for autodiff engine
 """
 
 # pylint: disable=missing-docstring
-# pylint: disable=invalid-name
 
 from pyfit.engine import Scalar
 
 
-def test_engine() -> None:
+def test_scalar() -> None:
     x = Scalar(1.0)
-    z = 2 * x + 2 + x
-    q = z + z * x
-    h = z * z
-    y = h + q + q * x
-    assert y.data == 45.0
+    y = (x * 2 + 1).relu()
+    assert y.data == 3
     y.backward()
-    assert x.grad == 62  # The numerical value of dy / dx
+    assert x.grad == 2  # dy/dx
+
+
+def test_scalar_sub() -> None:
+    x = Scalar(2.5)
+    y = Scalar(3.5)
+    z = x - y
+    assert z.data == -1
+    z.backward()
+    assert x.grad == 1  # dz/dx
+    assert y.grad == -1  # dz/dy
+
+
+def test_scalar_div() -> None:
+    x = Scalar(1.0)
+    y = Scalar(4.0)
+    z = x / y
+    assert z.data == 0.25
+    z.backward()
+    assert x.grad == 0.25  # dz/dx
+    assert y.grad == -0.0625  # dz/dy
