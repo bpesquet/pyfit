af275b164fa40c7cb510a72aae5c43092a32591b
bpesquet <bpesquet@gmail.com>
2020-04-18 11:15:58 +0200

    Add substraction tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -18,11 +18,38 @@ def test_scalar() -> None:
 def test_scalar_sub() -> None:
     x = Scalar(2.5)
     y = Scalar(3.5)
+
     z = x - y
     assert z.data == -1
     z.backward()
     assert x.grad == 1  # dz/dx
     assert y.grad == -1  # dz/dy
+
+    # Reset gradients
+    x.grad = 0
+    y.grad = 0
+
+    z = y - x
+    assert z.data == 1
+    z.backward()
+    assert x.grad == -1  # dz/dx
+    assert y.grad == 1  # dz/dy
+
+    # Reset gradient
+    x.grad = 0
+
+    z = x - 3
+    assert z.data == -0.5
+    z.backward()
+    assert x.grad == 1  # dz/dx
+
+    # Reset gradient
+    x.grad = 0
+
+    z = 3 - x
+    assert z.data == 0.5
+    z.backward()
+    assert x.grad == -1  # dz/dx
 
 
 def test_scalar_div() -> None:
