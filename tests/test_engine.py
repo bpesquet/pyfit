76f94fb1232a432aec5153059b9475fef77f5b8c
bpesquet <bpesquet@gmail.com>
2020-04-19 09:54:47 +0200

    Improve division tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -55,8 +55,35 @@ def test_scalar_sub() -> None:
 def test_scalar_div() -> None:
     x = Scalar(1.0)
     y = Scalar(4.0)
+
     z = x / y
     assert z.data == 0.25
     z.backward()
     assert x.grad == 0.25  # dz/dx
     assert y.grad == -0.0625  # dz/dy
+
+    # Reset gradients
+    x.grad = 0
+    y.grad = 0
+
+    z = y / x
+    assert z.data == 4
+    z.backward()
+    assert x.grad == -4  # dz/dx
+    assert y.grad == 1  # dz/dy
+
+    # Reset gradient
+    x.grad = 0
+
+    z = x / 3
+    assert z.data == 1 / 3
+    z.backward()
+    assert x.grad == 1 / 3  # dz/dx
+
+    # Reset gradient
+    x.grad = 0
+
+    z = 3 / x
+    assert z.data == 3
+    z.backward()
+    assert x.grad == -3  # dz/dx
